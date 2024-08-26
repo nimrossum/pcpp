da27a7aae2484e2f093bca73c8d0c3cad77945ec
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2024-08-26 10:53:15 +0200

    Solve 1.2
        Co-authored-by: AntonBertelsen <AntonBertelsen@users.noreply.github.com>
        Co-authored-by: PeterTheFuchs <PeterTheFuchs@users.noreply.github.com>

--- week01/code-exercises/anwers.md
+++ week01/code-exercises/anwers.md
@@ -3,3 +3,13 @@
 ## 1.1
 
 We get 19857118 and not 2000000
+
+## 1.2
+
+Now we get 200.
+How could this be?
+
+We are lucky. The threads are interleaved in such a way that the counter is incremented by one in each step. This is what we call a race condition.
+
+
+This is likely to happen with only 200 counts, but it is not gaurenteed, as we see in the example with more executions. Therefore we cannot assume that the result will be 200 every time.
--- week01/code-exercises/week01exercises/app/src/main/java/exercises01/TestLongCounterExperiments.java
+++ week01/code-exercises/week01exercises/app/src/main/java/exercises01/TestLongCounterExperiments.java
@@ -6,36 +6,36 @@ package exercises01;
 public class TestLongCounterExperiments {
 
     LongCounter lc = new LongCounter();
-    int counts = 10_000_000;
+    int counts = 100;
 
     public TestLongCounterExperiments() {
         Thread t1 = new Thread(() -> {
-                for (int i=0; i<counts; i++) 
+                for (int i=0; i<counts; i++)
                     lc.increment();
         });
         Thread t2 = new Thread(() -> {
-                for (int i=0; i<counts; i++) 
+                for (int i=0; i<counts; i++)
                     lc.increment();
         });
         t1.start(); t2.start();
         try { t1.join(); t2.join(); }
-        catch (InterruptedException exn) { 
+        catch (InterruptedException exn) {
             System.out.println("Some thread was interrupted");
         }
         System.out.println("Count is " + lc.get() + " and should be " + 2*counts);
     }
-    
+
     public static void main(String[] args) {
         new TestLongCounterExperiments();
     }
 
     class LongCounter {
-        private long count = 0;    
-     
+        private long count = 0;
+
         public void increment() {
             count = count + 1;
         }
-    
+
         public long get() {
             return count;
         }
