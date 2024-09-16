a1e2e38df66e81bbd14b177b6a6359ce0d6e1f95
Peter Berndt Fuchs <pefu@itu.dk>
2024-09-16 12:45:57 +0200

    solve ex. 4.1.1 and 4.1.2

--- week04/code-exercises/week04exercises/app/src/test/java/exercises04/ConcurrentSetTest.java
+++ week04/code-exercises/week04exercises/app/src/test/java/exercises04/ConcurrentSetTest.java
@@ -1,13 +1,26 @@
 package exercises04;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 // TODO: Very likely you need to expand the list of imports
 
 public class ConcurrentSetTest {
 
     // Variable with set under test
     private ConcurrentIntegerSet set;
-
+    private static int numThreads = Runtime.getRuntime().availableProcessors() * 2;
+    private static ExecutorService pool;
+    
     // TODO: Very likely you should add more variables here
 
 
@@ -20,7 +33,96 @@ public class ConcurrentSetTest {
         set = new ConcurrentIntegerSetBuggy();
         // set = new ConcurrentIntegerSetSync();
         // set = new ConcurrentIntegerSetLibrary();
+        
+        pool = Executors.newCachedThreadPool();
     }
 
     // TODO: Define your tests below
+    
+    @Test
+    public void TestTest() {
+        assertTrue(false);
+    }
+    
+    public static int xorShift() {
+        long y = System.nanoTime();
+        
+        y ^= (y << 6);
+        y ^= (y >>> 21);
+        y ^= (y << 7);
+        
+        return (int) y;
+    }
+    
+    @RepeatedTest(5000)
+    public void Test4_1_integerset_add_is_thread_safe() {
+        try{
+            CyclicBarrier cb = new CyclicBarrier(numThreads + 1);
+            
+            Runnable runnable = () -> {
+                try {
+                    cb.await();
+                    set.add(xorShift());
+                    cb.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+            
+            for (int i = 0; i < numThreads; i++) {
+                pool.execute(runnable);
+            }
+            
+            cb.await();
+            cb.await();
+            
+            assertTrue(set.size() == numThreads);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            pool.shutdown();
+        }
+    }
+    
+    @RepeatedTest(5000)
+    public void Test4_2_integerset_remove_is_thread_safe() {
+        try{
+            CyclicBarrier cb = new CyclicBarrier(numThreads + 1);
+            
+            set.add(1);
+            set.add(2);
+            set.add(3);
+            set.add(4);
+            
+            Runnable runnable = () -> {
+                try {
+                    cb.await();
+                    set.remove(1);
+                    set.remove(2);
+                    set.remove(3);
+                    set.remove(4);
+                    cb.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+            
+            for (int i = 0; i < numThreads; i++) {
+                pool.execute(runnable);
+            }
+            
+            cb.await();
+            cb.await();
+            
+            assertEquals(0,set.size());
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            pool.shutdown();
+        }
+    }
 }
