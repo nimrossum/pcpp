ab55c595bfc6e20bdea61638d0254add72a3d4f5
Jonas Nim Røssum <1959615+joglr@users.noreply.github.com>
2024-10-07 15:32:51 +0200

    Fix Exercise 3.1

--- week03/code-exercises/week03exercises/app/src/main/java/exercises03/BoundedBuffer.java
+++ week03/code-exercises/week03exercises/app/src/main/java/exercises03/BoundedBuffer.java
@@ -8,27 +8,41 @@ public class BoundedBuffer<T> implements BoundedBufferInteface<T>{
     private final LinkedList<T> queue;
     private final Semaphore filledSlots;
     private final Semaphore emptySlots;
+    private final Semaphore access;
 
     public BoundedBuffer(int size){
         queue = new LinkedList<>();
         filledSlots = new Semaphore(0);
         emptySlots = new Semaphore(size);
+        access = new Semaphore(1); // Make it so only a single thread can access the buffer at a time
     }
 
     @Override
     public T take() throws Exception {
 
         filledSlots.acquire(); // Wait for a filled slot
+        access.acquire(); // Wait for access to the buffer
 
-        emptySlots.release(); // Release an empty slot (Since we have emptied the slot we are taking from)
+        T item;
+        try {
+            item = queue.pop();
+        } finally {
+            access.release(); // Release access to the buffer
+            emptySlots.release(); // Release an empty slot (Since we have emptied the slot we are taking from)
+        }
 
-        return queue.pop(); // Pop the queue and return the item
+        return item; // Pop the queue and return the item
     }
 
     @Override
     public void insert(T elem) throws Exception {
         emptySlots.acquire(); // Wait for an empty slot to be available
-        queue.add(elem); // Add the item to the queue
-        filledSlots.release(); // Release a new filled slot
+        access.acquire(); // Wait for access to the buffer
+        try {
+            queue.add(elem); // Add the item to the queue
+        } finally {
+            access.release(); // Release access to the buffer
+            filledSlots.release(); // Release a new filled slot
+        }
     }
 }
