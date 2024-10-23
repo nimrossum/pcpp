7a077ee54da8830720795b305fa6c402d248b5b0
Jørgen Staunstrup <jst@itu.dk>
2024-10-23 07:58:49 +0200

    Preliminary version of week09 material

--- Week09/code-lecture/week09lecture/app/src/main/java/benchmarking/Benchmarkable.java
+++
@@ -1,7 +0,0 @@
-package benchmarking;
-import java.util.function.IntToDoubleFunction;
-
-public abstract class Benchmarkable implements IntToDoubleFunction {
-  public void setup() { }
-  public abstract double applyAsDouble(int i);
-}
