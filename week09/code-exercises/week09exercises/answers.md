4fc80a77a7900e16f7dac0e531deb29aee621536
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2024-10-28 11:33:20 +0100

    Solve 9.1.2

--- week09/code-exercises/week09exercises/answers.md
+++ week09/code-exercises/week09exercises/answers.md
@@ -7,3 +7,12 @@
 | 5  | 310,695,115.0  | 1,678,387.85| 2     |
 | 10 | 629,814,785.0  | 7,043,104.37| 2     |
 | 15 | 933,802,035.0  | 1,601,354.66| 2     |
+
+## 9.1.2
+
+The problem is that if two different threads were to attempt to transfer between the same accounts at the same time.
+
+Thread 1 wants to transfer from account 1 to account 2
+Thread 2 wants to transfer from account 2 to account 1
+
+If we just start by locking the source (Thread 1: 1, Thread 2: 2), each thread would lock and wait for other to be free, but it would never be free since we have created a deadlock. By having comparable id's we can ensure that the threads will always lock the accounts in the same order and therefore never step on each others toes.
--- week09/code-exercises/week09exercises/app/src/main/java/exercises09/ThreadsAccountExperimentsMany.java
+++ week09/code-exercises/week09exercises/app/src/main/java/exercises09/ThreadsAccountExperimentsMany.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class ThreadsAccountExperimentsMany {
 
-  static final int N = 10; 
+  static final int N = 10;
   static final int NO_TRANSACTION = 5;
   static final int NO_THREADS = 10;
   static final Account[] accounts = new Account[N];
   static final Thread[] threads = new Thread[NO_THREADS];
   static Random rnd = new Random();
-  
+
   public static void main(String[] args){ new ThreadsAccountExperimentsMany(); }
 
   public ThreadsAccountExperimentsMany(){
@@ -28,7 +28,7 @@ public class ThreadsAccountExperimentsMany {
       try {threads[i].join();} catch(Exception dummy){};
     }
   }
-  
+
   private static void doNTransactions(int noTransactions){
     for(int i = 0; i<noTransactions; i++){
       long amount = rnd.nextInt(5000)+100;
@@ -37,12 +37,12 @@ public class ThreadsAccountExperimentsMany {
       doTransaction( new Transaction( amount, accounts[source], accounts[target]));
     }
   }
-  
+
   private static void doTransaction(Transaction t){
     System.out.println(t);
     t.transfer();
   }
-  
+
   static class Transaction {
     final Account source, target;
     final long amount;
@@ -51,10 +51,12 @@ public class ThreadsAccountExperimentsMany {
       this.source = source;
       this.target = target;
     }
-    
+
     public void transfer(){
       Account min = accounts[Math.min(source.id, target.id)];
       Account max = accounts[Math.max(source.id, target.id)];
+      // Account s = accounts[source.id];
+      // Account t = accounts[target.id];
       synchronized(min){
         synchronized(max){
           source.withdraw(amount);
@@ -63,7 +65,7 @@ public class ThreadsAccountExperimentsMany {
         }
       }
     }
-    
+
     public String toString(){
       return "Transfer " + amount + " from " + source.id + " to " + target.id;
     }
@@ -74,7 +76,7 @@ public class ThreadsAccountExperimentsMany {
     public final int id;
     private long balance = 0;
     Account( int id ){ this.id = id;}
-    public void deposit(long sum){ balance += sum; } 
+    public void deposit(long sum){ balance += sum; }
     public void withdraw(long sum){ balance -= sum; }
     public long getBalance(){ return balance; }
   }
